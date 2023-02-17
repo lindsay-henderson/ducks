97c6170109bbb7eedb87d914e55e6d5da6b92f12
LindsayH <lindsaynhenderson@gmail.com>
2023-02-17 12:43:51 -0600

    build form, disable submit button if data is not valid

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = ({ user, handleLogout}) => {
     <ul>
       <li><NavLink to="/login">Log In</NavLink></li>
       <li><NavLink to="/signup">Sign Up</NavLink></li>
-      <li><NavLink to="/add-client"></NavLink></li>
+      <li><NavLink to="/add-client">Add New Client</NavLink></li>
     </ul>
   )
 
--- src/pages/AddClient/AddClient.jsx
+++ src/pages/AddClient/AddClient.jsx
@@ -1,10 +1,29 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 
 function AddClient(props) {
+  const[formData, setFormData] = useState({
+    name: "",
+    email:"",
+  })
+  
+  const [validForm, setValidForm] = useState(false)
+  
+  const formElement = useRef()
+  
+  
+	const handleChange = evt => {
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+	}
+  
+    useEffect(() => {
+      formElement.current.checkValidity() ? setValidForm(true) : setValidForm(false)
+    }, [formData])
+    
+
 	return (
 		<>
 			<h1>Add Client</h1>
-			<form autoComplete="off">
+			<form autoComplete="off" ref={formElement}>
 				<div className="form-group mb-3">
 					<label htmlFor="name-input" className="form-label">
 						Name
@@ -14,6 +33,8 @@ function AddClient(props) {
 						className="form-control"
 						id="name-input"
 						name="name"
+            value={formData.name}
+            onChange={handleChange}
 						required
 					/>
 				</div>
@@ -26,6 +47,8 @@ function AddClient(props) {
 						className="form-control"
 						id="email-input"
 						name="email"
+            value={formData.email}
+            onChange={handleChange}
 						required
 					/>
 				</div>
@@ -33,6 +56,7 @@ function AddClient(props) {
 					<button
 						type="submit"
 						className="btn btn-primary btn-fluid"
+            disabled={!validForm}
 					>
 						Add Client
 					</button>
